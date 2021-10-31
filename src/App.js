485e20da6e8244363188a0415f35ebf9c6d3cc7a
jleomorris <james-morris2010@hotmail.co.uk>
2021-10-31 01:41:57 +0100

    Add animation to Loader

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import { useEffect, useState } from "react";
-import "./sass/main.scss";
-
+import { useEffect, useState } from 'react';
+import './sass/main.scss';
 // Components
-import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Loader from "./components/Loader";
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Loader from './components/Loader';
+// Animation
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     loading
-      ? document.querySelector("body").classList.add("loading")
-      : document.querySelector("body").classList.remove("loading");
+      ? document.querySelector('body').classList.add('loading')
+      : document.querySelector('body').classList.remove('loading');
   }, [loading]);
 
   return (
-    <>
+    <AnimatePresence>
       {loading ? (
-        <Loader setLoading={setLoading} />
+        <motion.div key='loader'>
+          <Loader setLoading={setLoading} />
+        </motion.div>
       ) : (
         <>
           <Header />
           <Banner />
           {!loading && (
-            <div className="transition-image final">
+            <div className='transition-image final'>
               <img src={process.env.PUBLIC_URL + `/images/image-2.jpg`} />
             </div>
           )}
         </>
       )}
-    </>
+    </AnimatePresence>
   );
 }
 
